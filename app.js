df68d2c7a3590973eceb3283d5249b644ba171ec
Omar Ochoa <omar@ochoawebdesign.com>
2022-08-03 14:42:36 -0700

    Script itemDeleteSubmit

--- app.js
+++ app.js
@@ -73,6 +73,18 @@ const ItemCtrl = (function(){
 
             return found;
         },
+        deleteItem: function(id) {
+            // Get IDs
+            ids = data.items.map(function(item){
+                return item.id;
+            });
+
+            // Get Index
+            const index = ids.indexOf(id);
+
+            // Remove Item
+            data.items.splice(index, 1);
+        },
         setCurrentItem: function(item){
             data.currentItem = item;
         },
@@ -165,6 +177,11 @@ const UICtrl = (function(){
                 }
             });
         },
+        deleteListItem: function(id){
+            const itemID = `#item-${id}`;
+            const item = document.querySelector(itemID);
+            item.remove();
+        },
         clearInput: function() {
             document.querySelector(UISelectors.itemNameInput).value = '';
             document.querySelector(UISelectors.itemCaloriesInput).value = '';
@@ -223,6 +240,9 @@ const App = (function(ItemCtrl, UICtrl){
 
         // Update item event
         document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
+        // Delete item event
+        document.querySelector(UISelectors.deleteBtn).addEventListener('click', itemDeleteSubmit);
 
         // Back button event
         document.querySelector(UISelectors.backBtn).addEventListener('click', function(e){
@@ -295,6 +315,22 @@ const App = (function(ItemCtrl, UICtrl){
 
         UICtrl.clearEditState();
 
+        e.preventDefault();
+    }
+
+    // Delete Button Event
+    const itemDeleteSubmit = function(e) {
+        // Get Current Item ID
+        const currentItem = ItemCtrl.getCurrentItem();
+        // Delete from data structure
+        ItemCtrl.deleteItem(currentItem.id);
+        // Delete from UI
+        UICtrl.deleteListItem(currentItem.id);
+
+        // Clear state and change calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        UICtrl.showTotalCalories(totalCalories);
+        UICtrl.clearEditState();
         e.preventDefault();
     }
 
